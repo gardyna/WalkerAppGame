babd11feec70bd222149ae28cffc20266faea209
Dagur Arinbjörn Daníelsson <dagurdan@simnet.is>
2015-10-01 12:44:33 +0000

    showing progress bar to next level

--- app/src/main/java/com/ru/andr/walkinggame/StepCount.java
+++ app/src/main/java/com/ru/andr/walkinggame/StepCount.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class StepCount extends AppCompatActivity implements SensorEventListener {
@@ -16,6 +17,7 @@ public class StepCount extends AppCompatActivity implements SensorEventListener 
     private TextView textView;
     private TextView levelText;
     private TextView nextLevelText;
+    private ProgressBar levelProgress;
 
     private SensorManager mSenssorManager;
 
@@ -35,6 +37,7 @@ public class StepCount extends AppCompatActivity implements SensorEventListener 
         textView = (TextView)findViewById(R.id.textview);
         levelText = (TextView)findViewById(R.id.leveltext);
         nextLevelText = (TextView)findViewById(R.id.tonextlevel);
+        levelProgress = (ProgressBar)findViewById(R.id.progressBar);
 
         mSenssorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         mStepDetectorSensor = mSenssorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
@@ -101,6 +104,8 @@ public class StepCount extends AppCompatActivity implements SensorEventListener 
             mStepsTaken = value;
             levelText.setText("LVL: " + player.getLevel());
             nextLevelText.setText("EXP to LVL: " + player.getExpToNextLevel());
+            levelProgress.setMax(player.getExpToNextLevel());
+            levelProgress.setProgress(player.getEXP());
 
         }/*else if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR){
             textView.setText("Step detector detected : " + value);
